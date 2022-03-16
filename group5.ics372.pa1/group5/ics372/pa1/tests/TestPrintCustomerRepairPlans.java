a9ccd87c6b66976c811125f355f954f04c81a84b
AnthonyFlowers <AnthonyFlowers@users.noreply.github.com>
2022-03-15 22:09:47 -0500

    Updated PrintCustomerRepairPlans test

---
+++ group5.ics372.pa1/group5/ics372/pa1/tests/TestPrintCustomerRepairPlans.java
@@ -0,0 +1,29 @@
+package group5.ics372.pa1.tests;
+
+import group5.ics372.pa1.Appliance;
+import group5.ics372.pa1.ClothDryer;
+import group5.ics372.pa1.Company;
+import group5.ics372.pa1.Customer;
+
+public class TestPrintCustomerRepairPlans {
+
+	public static void main(String[] args) {
+		testAll();
+	}
+
+	private static void testAll() {
+		testPrintCustomerRepairPlans();
+	}
+
+	private static void testPrintCustomerRepairPlans() {
+		Company company = new Company();
+		Customer customer = new Customer(1, "Anthony", "Address", "Phone");
+		Appliance appliance = new ClothDryer(1, "Dryer Brand", "Dryer Model", 300.0, 15.0);
+		company.addCustomer(customer);
+		company.addAppliance(appliance);
+		company.enrollCustomerInRepairPlan(1, 1);
+		company.enrollCustomerInRepairPlan(1, 1);
+		company.enrollCustomerInRepairPlan(1, 1);
+		company.printCustomerRepairPlans();
+	}
+}
